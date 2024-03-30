277035e3f6b15c893bd2859d765386e53e0aff50
William B. Swift <willbswift@hotmail.com>
2024-03-29 23:21:28 -0500

    RPG update

--- javascript/rooscript.js
+++ javascript/rooscript.js
@@ -1,21 +1,63 @@
 
 //load stuff
 $(document).ready(function () {
-
   $(".main").on("click", function(event){
-
     $(this).toggleClass("active");
   })
-
-
-
-
 });
 
-
-
-
-
-
-
+$(function(){
+  $("#navbar").load("navbar.html"); 
+});
+$(function(){
+  $("#footer").load("footer.html"); 
+});
+$(function(){
+  $("#gallifreynav").load("roogallifreynav.html"); 
+});
+$(function(){
+  $("#biologynav").load("roobiologynav.html"); 
+});
+$(function(){
+  $("#lifedeathnav").load("roolifedeathnav.html"); 
+});
+$(function(){
+  $("#governmentnav").load("roogovernmentnav.html"); 
+});
+$(function(){
+  $("#webnav").load("roowebnav.html"); 
+});
+$(function(){
+  $("#timelordnav").load("rootimelordnav.html"); 
+});
+$(function(){
+  $("#timeline1nav").load("rootimeline1nav.html"); 
+});
+$(function(){
+  $("#timeline2nav").load("rootimeline2nav.html"); 
+});
+$(function(){
+  $("#timeline3nav").load("rootimeline3nav.html"); 
+});
+$(function(){
+  $("#timeline4nav").load("rootimeline4nav.html"); 
+});
+$(function(){
+  $("#timeline5nav").load("rootimeline5nav.html"); 
+});
+$(function(){
+  $("#timeline6nav").load("rootimeline6nav.html"); 
+});
+$(function(){
+  $("#timeline7nav").load("rootimeline7nav.html"); 
+});
+$(function(){
+  $("#timeline8nav").load("rootimeline8nav.html"); 
+});
+$(function(){
+  $("#childrennav").load("roochildrennav.html"); 
+});
+$(function(){
+  $("#appendixnav").load("rooappendixnav.html"); 
+});
 
